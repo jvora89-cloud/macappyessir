fc6c6848dabd2d1d918c7a953e2720582a6a4d33
jay <jvora89@yahoo.com>
2026-02-01 17:16:15 -0500

    Initial Commit

---
+++ macappyessir/macappyessirApp.swift
@@ -0,0 +1,17 @@
+//
+//  macappyessirApp.swift
+//  macappyessir
+//
+//  Created by Jay Vora on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct macappyessirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macappyessirTests/macappyessirTests.swift
@@ -0,0 +1,17 @@
+//
+//  macappyessirTests.swift
+//  macappyessirTests
+//
+//  Created by Jay Vora on 2/1/26.
+//
+
+import Testing
+@testable import macappyessir
+
+struct macappyessirTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
